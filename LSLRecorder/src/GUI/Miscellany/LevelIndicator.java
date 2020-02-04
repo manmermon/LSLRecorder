5f6b42906ece2226054a655a8078ef8fd259de64
manmermon@dte.us.es <manmermon@dte.us.es>
2020-02-04 12:34:28 +0100

    Beta 2020.02.04.2

--- LSLRecorder/src/GUI/Miscellany/LevelIndicator.java
+++ LSLRecorder/src/GUI/Miscellany/LevelIndicator.java
@@ -1,22 +1,25 @@
 /*
- * Copyright 2011-2013 by Manuel Merino Monge <manmermon@dte.us.es>
+ * Work based on CLIS by Manuel Merino Monge <https://github.com/manmermon/CLIS>
+ * 
+ * Copyright 2020 by Manuel Merino Monge <manmermon@dte.us.es>
  *  
- *   This file is part of CLIS.
+ *   This file is part of LSLRec.
  *
- *   CLIS is free software: you can redistribute it and/or modify
+ *   LSLRec is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   CLIS is distributed in the hope that it will be useful,
+ *   LSLRec is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with CLIS.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with LSLRec.  If not, see <http://www.gnu.org/licenses/>.
  *   
  */
+
 
 package GUI.Miscellany;
 
